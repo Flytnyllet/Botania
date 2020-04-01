91827877c60a935c0e5a9c4ed9ee09fb98e95326
PretentiousPanda <42602418+PretentiousPanda@users.noreply.github.com>
2020-04-01 12:45:24 +0200

    Derp

--- Assets/Programmera/Jens/In Progress Scripts/InspectorLineInput.cs
+++ Assets/Programmera/Jens/In Progress Scripts/InspectorLineInput.cs
@@ -2,17 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-[CustomEditor(typeof(LineInput))]
-public class InspectorLineInput : Editor
+//[CustomEditor(typeof(LineInput))]
+public class InspectorLineInput : EditorWindow
 {
-    public override void OnInspectorGUI()
-    {
-        DrawDefaultInspector();
+    [SerializeField] string _input;
 
-        LineInput myScript = (LineInput)target;
-        if (GUILayout.Button("Build Object"))
-        {
-            myScript.Engage();
-        }
+    // Add menu item named "My Window" to the Window menu
+    [MenuItem("Window/Input")]
+    public static void ShowWindow()
+    {
+        //Show existing window instance. If one doesn't exist, make one.
+        EditorWindow.GetWindow(typeof(InspectorLineInput));
     }
+
+    void OnGUI()
+    {
+        EditorGUILayout.TextField("Text Field", _input);
+        if (GUILayout.Button("Build Object"))
+            {
+                //myScript.Engage();
+                GUI.FocusControl(null);
+                Repaint();
+            }
+    }
+
+
+    //public override void OnInspectorGUI()
+    //{
+    //    DrawDefaultInspector();
+
+    //    LineInput myScript = (LineInput)target;
+    //    if (GUILayout.Button("Build Object"))
+    //    {
+    //        myScript.Engage();
+    //        GUI.FocusControl(null);
+    //        Repaint();
+    //    }
+    //}
+
 }
