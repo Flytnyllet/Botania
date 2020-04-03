9b6a0053897d1fc4e20e8a85a388aecdd5fac20e
JensLars <larssonjenz@gmail.com>
2020-04-03 20:02:30 +0200

    Just work, you fuck!

---
+++ Assets/Scripts/Events/EventHolder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+[System.Serializable]
+public class EventBox
+{
+    public string subscriptionName;
+    public UnityEvent action;
+    public void TriggerEvent(EventParameter eventParam)
+    {
+        action.Invoke();
+    }
+}
+
+public class EventHolder : MonoBehaviour
+{
+    public EventBox[] events;
+    private void OnEnable()
+    {
+        for (int i = 0; i < events.Length; i++)
+        {
+            EventManager.Subscribe(events[i].subscriptionName, events[i].TriggerEvent);
+        }
+    }
+    private void OnDisable()
+    {
+        for (int i = 0; i < events.Length; i++)
+        {
+            EventManager.UnSubscribe(events[i].subscriptionName, events[i].TriggerEvent);
+        }
+    }
+}
---
+++ Assets/Scripts/Events/EventTrigger.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventTrigger : MonoBehaviour
+{
+    [Header("Trigger Options")]
+    [SerializeField] Interactions interaction = Interactions.onButtonPress;
+    enum Interactions { onEnter = 0, onButtonPress, onLeave };
+    //[SerializeField] InteractionType interactionType = InteractionType.Direct;
+    //public enum InteractionType { Direct = 0, PreAndPostTimer };
+    [SerializeField] Actions postTriggerAction = Actions.Nothing;
+    enum Actions { Nothing = 0, DeleteObject, DeactivateTrigger };
+
+    [Header("Event Data")]
+    public string[] eventCalls = new string[1];
+    public EventParameter eventParameter;
+    public UnityEvent action;
+
+    void Engage()
+    {
+        foreach (string eventCall in eventCalls)
+        {
+            if (eventCall != "")
+            {
+                    EventManager.TriggerEvent(eventCall, eventParameter);
+                    action.Invoke();
+            }
+            switch (postTriggerAction)
+            {
+                case Actions.DeleteObject:
+                    Destroy(this.gameObject);
+                    break;
+
+                case Actions.DeactivateTrigger:
+                    Destroy(this);
+                    break;
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (interaction == Interactions.onEnter && collision.tag == "Player")
+        {
+            Engage();
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (interaction == Interactions.onEnter && collision.gameObject.tag == "Player")
+        {
+            Engage();
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (interaction == Interactions.onButtonPress && collision.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            Engage();
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (interaction == Interactions.onButtonPress && collision.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            Engage();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (interaction == Interactions.onLeave && collision.gameObject.tag == "Player")
+        {
+            Engage();
+        }
+    }
+}
