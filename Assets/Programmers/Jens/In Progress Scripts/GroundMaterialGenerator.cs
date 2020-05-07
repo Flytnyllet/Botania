f42863b3359de9a0a74c4f09f5d327b8ee30d769
JensLars <larssonjenz@gmail.com>
2020-05-07 14:30:54 +0200

    asdadsf

--- Assets/Programmers/Jens/In Progress Scripts/GroundMaterialGenerator.cs
+++ Assets/Programmers/Jens/In Progress Scripts/GroundMaterialGenerator.cs
@@ -12,17 +12,29 @@ public class GroundMaterialGenerator : ScriptableObject
     [SerializeField] Shader _shader;
     [SerializeField] int _noiseDetailLevel = 1;
     [SerializeField] Texture2D _mainTex;
-    [SerializeField][Range(0,1)] float _mainTexStart = 0.8f;
     [SerializeField] Texture2D _emission;
-    [SerializeField] Layer _layer;
+    [SerializeField] [Range(0, 1)] float _mainTexStart = 0.8f;
+    [SerializeField] [Range(0, 1)] float _mainTexStop = 0.7f;
+    [SerializeField] float _mainTexScale = 4f;
+    const int LAYER_COUNT = 4;
+    [SerializeField] Layer[] _layers = new Layer[LAYER_COUNT];
     //[SerializeField] Material _material;
     // Update is called once per frame
     object obj;
 
+    void OnValidate()
+    {
+        if (_layers.Length != LAYER_COUNT)
+        {
+            Debug.LogWarning("Don't change the '_layers' field's array size!");
+            Array.Resize(ref _layers, LAYER_COUNT);
+        }
+    }
+
     public Material MakeMaterial(int size, Vector2 pos, MeshRenderer renderer)
     {
         Material _material = new Material(_shader);
-        Texture2D noiseTex = new Texture2D(size, size, TextureFormat.R8, false);
+        Texture2D noiseTex = new Texture2D(size, size, TextureFormat.RGBA32, false);
         noiseTex.wrapMode = TextureWrapMode.Clamp;
         var colour = noiseTex.GetRawTextureData();
         Color[] colorMap = new Color[1];
@@ -32,45 +44,62 @@ public class GroundMaterialGenerator : ScriptableObject
             {
 
                 colorMap = new Color[size * size];
-                float[,] noise = Noise.GenerateNoiseMap(size, size, _noiseDetailLevel, _layer.GetNoise.NoiseSettingsDataMerge, pos);
+                float[][,] noises = new float[LAYER_COUNT][,];
+                for (int i = 0; i < LAYER_COUNT; i++)
+                {
+                    if (_layers[i].GetNoise != null)
+                    {
+                        noises[i] = Noise.GenerateNoiseMap(size, size, _noiseDetailLevel, _layers[i].GetNoise.NoiseSettingsDataMerge, pos);
+                    }
+                    else
+                    {
+                        noises[i] = new float[size, size];
+                    }
+                }
                 //byte[] noiseByte = new byte[size * size];
                 for (int y = 0; y < size; y++)
                 {
-                    for (int x = 0; x < size; x++)
-                    {
-                        // colour[y * size + x] = (byte)(255 * noise[x, y]);
-                        colorMap[y * size + x] = Color.Lerp(Color.black, Color.white, Mathf.InverseLerp(0, 1, noise[x, y]));
-
+                for (int x = 0; x < size; x++)
+                {
+                    //if (noises[0][x, y] < 0 || noises[0][x, y] > 1) ThreadedDataRequester.AddToCallbackQueue(() => { Debug.LogError("Noise Value out of range"); });
+                    //ThreadedDataRequester.AddToCallbackQueue(() => { Debug.Log(noises[1][x, y]); });
+                            // colour[y * size + x] = (byte)(255 * noise[x, y]);
+                            colorMap[y * size + x].r = noises[0][x, y] * 255;
+                        colorMap[y * size + x].g = noises[1][x, y] * 255;
+                        colorMap[y * size + x].b = noises[2][x, y] * 255;
+                        colorMap[y * size + x].a = noises[3][x, y] * 255;
                     }
                 }
                 //colour = noiseByte;
                 //Debug.Log(noiseByte[1]);
                 ThreadedDataRequester.AddToCallbackQueue(() =>
-                {
-                    //noiseTex.LoadRawTextureData(colour);
-                    noiseTex.SetPixels(colorMap);
-                    noiseTex.Apply();
-                    //MaterialPropertyBlock prop = new MaterialPropertyBlock();
-                    //prop.SetTexture("_NoiseTextures", noiseTex);
-                    //renderer.SetPropertyBlock(prop);
-                    _material.SetTexture("_NoiseTextures", noiseTex);
-                });
+                                {
+                                    //noiseTex.LoadRawTextureData(colour);
+                                    noiseTex.SetPixels(colorMap);
+                                    noiseTex.Apply();
+                                    //MaterialPropertyBlock prop = new MaterialPropertyBlock();
+                                    //prop.SetTexture("_NoiseTextures", noiseTex);
+                                    //renderer.SetPropertyBlock(prop);
+                                    _material.SetTexture("_NoiseTextures", noiseTex);
+                                });
             }
             catch (Exception e)
             {
                 Debug.LogError(e);
             }
         });
-        _material.SetTexture("_AltTex", _layer.GetTexture());
+        _material.SetTexture("_AltTex0", _layers[0].GetTexture ?? new Texture2D(0, 0));
+        _material.SetTexture("_AltTex1", _layers[1].GetTexture ?? new Texture2D(0, 0));
+        _material.SetTexture("_AltTex2", _layers[2].GetTexture ?? new Texture2D(0, 0));
+        _material.SetTexture("_AltTex3", _layers[3].GetTexture ?? new Texture2D(0, 0));
         _material.SetTexture("_MainTex", _mainTex);
         _material.SetTexture("_Emission", _emission);
-        _material.SetColor("baseColor", _layer.GetTint());
-        _material.SetFloat("baseTextureScale", _layer.GetTextureScale());
-        _material.SetFloat("mainTexStop", _layer.GetMainStop());
+        _material.SetFloat("baseTextureScale", _mainTexScale);
+        _material.SetFloatArray("baseTextureScale", _layers.Select(x => x.GetTextureScale).ToArray());
+        _material.SetFloat("mainTexStop", _mainTexStop);
         _material.SetFloat("mainTexStart", _mainTexStart);
 
         return _material;
-
     }
 
     [System.Serializable]
@@ -78,17 +107,11 @@ public class GroundMaterialGenerator : ScriptableObject
     {
         [SerializeField] NoiseSettingsData _noise;
         [SerializeField] Texture2D _texture;
-        [SerializeField] [Range(0, 1)] float _mainTexStop;
-        [SerializeField, Range(0, 1)] float _textureStrenght;
-        [SerializeField] Color _tint;
-        [SerializeField, Range(0, 1)] float _textureScale;
+        [SerializeField] float _textureScale;
 
         public NoiseSettingsData GetNoise { get => _noise; }
-        public Texture2D GetTexture() { return _texture; }
-        public Color GetTint() { return _tint; }
-        public float GetTextureScale() { return _textureScale; }
-        public float GetTextureStrenght() { return _textureStrenght; }
-        public float GetMainStop() { return _mainTexStop; }
+        public Texture2D GetTexture { get => _texture; }
+        public float GetTextureScale { get => _textureScale; }
     }
 
 }
--- Assets/Scripts/Shaders/Terrain.shader
+++ Assets/Scripts/Shaders/Terrain.shader
@@ -3,9 +3,8 @@
 	Properties
 	{
 		[PerRendererData]_MainTex("Texture", 2D) = "white" {}
-		[PerRendererData]_AltTex("Texture", 2D) = "white" {}
 		[PerRendererData]_Emission("Texture", 2D) = "white" {}
-		[PerRendererData]_NoiseTextures("Texture", 2D) = "white" {}
+		[PerRendererData]_NoiseTexture("Texture", 2D) = "white" {}
 	}
 		SubShader
 	{
@@ -19,27 +18,27 @@
 		// Use shader model 3.0 target, to get nicer looking lighting
 		#pragma target 3.0
 
-		const static float EPSILON = 1E-4;
+		#define EPSILON = 1E-4;
+		const static  int LAYER_COUNT = 4;
 
 		sampler2D _MainTex;
-		sampler2D _AltTex;
-		sampler2D _Emission;
-		sampler2D _NoiseTextures;
-
-		float3 baseColor;
 		float baseTextureScale;
+		sampler2D _Emission;
+		sampler2D _AltTex0;
+		sampler2D _AltTex1;
+		sampler2D _AltTex2;
+		sampler2D _AltTex3;
+		sampler2D _NoiseTexture;
+
 		float mainTexStart;
 		float mainTexStop;
-
-		//float minHeight;
-		//float maxHeight;
 
 		float random(float2 st) {
 			return frac(sin(dot(st.xy,
 				float2(12.9898, 78.233)))
 				* 43758.5453123);
 		}
-		float noise(float2 xy) {
+		float Noise(float2 xy) {
 			float2 i = floor(xy);
 			float2 f = frac(xy);
 
@@ -54,7 +53,7 @@
 				(c - a)* u.y * (1.0 - u.x) +
 				(d - b) * u.x * u.y;
 		}
-		float noise(float x, float y) {
+		float Noise(float x, float y) {
 			float2 i = floor(float2(x, y));
 			float2 f = frac(float2(x, y));
 
@@ -105,29 +104,15 @@
 				float3 blendAxes = abs(IN.worldNormal);
 				blendAxes /= blendAxes.x + blendAxes.y + blendAxes.z;
 				float3 scalesPos = IN.worldPos / 2;
-				float  noiseVal =1-smoothstep(0.01,0.2,noise(IN.worldPos.xz/4));
-				//float noiseStrenght = tex2D(_NoiseTextures, IN.uv_MainTex).x*baseTextureStrenght[0];
-				float noiseStrenght = tex2D(_NoiseTextures, IN.uv_MainTex).x;
-				//int2 pixelCoord = IN.uv_MainTex*_NoiseSize;
-				//float noiseStrenght = smoothstep(0.01 ,0.05 , _NoiseArray[pixelCoord.x, pixelCoord.y].x);
-				float3 altCol = tex2D(_AltTex, scalesPos.xz / 4);
-				//float3 altCol = triplanar(_AltTex, IN.worldPos, 2, blendAxes);
+				float  noiseVal = 1 - smoothstep(0.01,0.2,Noise(IN.worldPos.xz / 4));
+				float4 noise = tex2D(_NoiseTexture, IN.uv_MainTex);
+				float3 altCol = tex2D(_AltTex0, scalesPos.xz / 4);
 				float3 colour = tex2D(_MainTex, scalesPos.xz / 4);
-				//float3 colour = triplanar(_MainTex, IN.worldPos, 2, blendAxes);
 				float3 emissions = tex2D(_Emission, scalesPos.xz / 4);
-				//for (int i = 0; i < layerCount; i++) {
-				//	//float drawStrength = inverseLerp();
-
-				//	//float3 baseColor = baseColors[i] * baseColorStrength[i];
-				//	//float3 textureColor = triplanar(float3(IN.worldPos), baseTextureScales[i], blendAxes, i)*noiseStrenght;
-				//	float3 textureColor = triplanar(float3(IN.worldPos), baseTextureScales[i], blendAxes, i);
-
-				//	o.Albedo = colour;
-				//}
-				o.Albedo = lerp(altCol, colour, smoothstep(mainTexStart, mainTexStop, noiseStrenght));
-				o.Emission = lerp( float3(0,0,0), emissions, smoothstep(0.815, 0.825, noiseStrenght))*noiseVal;
+				o.Albedo = lerp(altCol, colour, smoothstep(mainTexStart, mainTexStop, noise.x));
+				o.Emission = lerp(float3(0,0,0), emissions, smoothstep(0.815, 0.825, noise.x))*noiseVal;
 				//o.Albedo = altCol+colour;
-				//o.Albedo = float4(noiseStrenght, 0,0,1);
+				o.Albedo = noise;
 				//o.Albedo = float4(IN.uv_MainTex,0,1);
 			}
 			ENDCG
