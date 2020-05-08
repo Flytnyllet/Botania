1686f5dd775d4f280da67759d1ea89187b0bf89f
Filip Petersson <zmarfan@hotmail.com>
2020-05-08 14:26:45 +0200

    Added fog and implemented things

---
+++ Assets/Programmers/Filip/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    static GameManager _thisGameManager;
+
+    private void Awake()
+    {
+        if (_thisGameManager == null)
+        {
+            _thisGameManager = this;
+        }
+        else
+            Destroy(gameObject);
+    }
+}
