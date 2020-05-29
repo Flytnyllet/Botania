16473df6322cfd4f692df37078008cd721f6d10e
Filip Petersson <zmarfan@hotmail.com>
2020-05-29 17:40:24 +0200

    Added Quality setting

---
+++ Assets/Programmers/Filip/Scripts/Menu/GraphicsQuality.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Linq;
+
+public class GraphicsQuality : MonoBehaviour
+{
+    TMP_Dropdown _dropdown;
+
+    private void Awake()
+    {
+        _dropdown = GetComponent<TMP_Dropdown>();
+        AddOptions();
+    }
+
+    private void AddOptions()
+    {
+        List<string> options = QualitySettings.names.ToList<string>();
+
+        _dropdown.AddOptions(options);
+        _dropdown.SetValueWithoutNotify(QualitySettings.GetQualityLevel());
+    }
+
+    public void ChangeQuality()
+    {
+        QualitySettings.SetQualityLevel(_dropdown.value);
+    }
+}
