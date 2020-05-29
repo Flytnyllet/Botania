af2cc23649a196d9aedea6af283aa283d55f0fa4
Filip Petersson <zmarfan@hotmail.com>
2020-05-29 16:20:25 +0200

    Fixed resolution and fov

---
+++ Assets/Programmers/Filip/Scripts/ChangeResolution.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Linq;
+
+public class ChangeResolution : MonoBehaviour
+{
+    TMP_Dropdown _dropdown;
+
+    private void Awake()
+    {
+        _dropdown = GetComponent<TMP_Dropdown>();
+        AddResolutionAsOptions();
+    }
+
+    private void AddResolutionAsOptions()
+    {
+        Resolution currentResolution = Screen.currentResolution;
+
+        Resolution[] resolutions = (Resolution[])Screen.resolutions.Select(resolution => new Resolution { width = resolution.width, height = resolution.height }).Distinct().ToArray();
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            options.Add(resolutions[i].width + " x " + resolutions[i].height);
+
+            if (resolutions[i].width == currentResolution.width && resolutions[i].height == currentResolution.height)
+                currentResolutionIndex = i;
+        }
+
+        _dropdown.AddOptions(options);
+        _dropdown.SetValueWithoutNotify(currentResolutionIndex);
+    }
+
+    public void DoChangeResolution()
+    {
+        Resolution[] resolutions = (Resolution[])Screen.resolutions.Select(resolution => new Resolution { width = resolution.width, height = resolution.height }).Distinct().ToArray();
+
+        Screen.SetResolution(resolutions[_dropdown.value].width, resolutions[_dropdown.value].height, Screen.fullScreen);
+    }
+}
--- Assets/Programmers/Filip/Scripts/FOVSetting.cs
+++ Assets/Programmers/Filip/Scripts/FOVSetting.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class FOVSetting : MonoBehaviour
 {
+    [SerializeField] TMP_Text _value;
     Slider _slider;
 
     private void Awake()
@@ -12,8 +14,15 @@ public class FOVSetting : MonoBehaviour
         _slider = GetComponent<Slider>();
     }
 
+    private void OnEnable()
+    {
+        _slider.value = Player.GetPlayerCamera().fieldOfView;
+        ChangeFOV();
+    }
+
     public void ChangeFOV()
     {
         Player.GetPlayerCamera().fieldOfView = _slider.value;
+        _value.text = ((int)_slider.value).ToString();
     }
 }
