c2a73814dfcb3eca4c3a0cbc9a783a078e92efb5
alliebeans <47175901+alliebeans@users.noreply.github.com>
2020-05-31 09:02:11 +0200

    Amb_Rain_Overrider | FMOD | etc

---
+++ Assets/Audio/Albin/Scripts/Audio/Amb/Amb_Rain/Amb_Rain_Overrider.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Amb_Rain_Overrider : MonoBehaviour
+{
+    private SphereCollider _trigger;
+
+    private void Awake()
+    {
+        _trigger = GetComponent<SphereCollider>();
+        if (_trigger == null) { Destroy(gameObject); }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Amb_Rain.Instance.Set_Rain_Override(0.85f);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Player.GetCurrentBiome() == BiomeTypes.BIRCH)
+            {
+                Amb_Rain.Instance.Set_Rain_Override(0.7f);
+            }
+            else
+            {
+                Amb_Rain.Instance.Set_Rain_Override(0);
+            }
+        }
+    }
+}
