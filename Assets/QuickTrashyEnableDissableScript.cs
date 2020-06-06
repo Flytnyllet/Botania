2e7477d68dc41521c69860f73ac8b9bae59d2154
PretentiousPanda <42602418+PretentiousPanda@users.noreply.github.com>
2020-06-06 16:40:56 +0200

    asd

---
+++ Assets/QuickTrashyEnableDissableScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickTrashyEnableDissableScript : MonoBehaviour
+{
+    [SerializeField] GameObject[] _target;
+    private void OnEnable()
+    {
+        EventManager.Subscribe(EventNameLibrary.OPEN_BOOK, SetEnabled);
+        EventManager.Subscribe(EventNameLibrary.CLOSE_BOOK, SetDissabled);
+    }
+    private void OnDisable()
+    {
+        EventManager.UnSubscribe(EventNameLibrary.OPEN_BOOK, SetEnabled);
+        EventManager.UnSubscribe(EventNameLibrary.CLOSE_BOOK, SetDissabled);
+    }
+    void SetEnabled(EventParameter param = null)
+    {
+        foreach (GameObject gObject in _target)
+        {
+            gObject.SetActive(true);
+        }
+    }
+    void SetDissabled(EventParameter param = null)
+    {
+        foreach (GameObject gObject in _target)
+        {
+            gObject.SetActive(false);
+        }
+    }
+}
